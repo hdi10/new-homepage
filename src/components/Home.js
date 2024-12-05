039057cc1a9e940d83afbb9662c574a2b364e332
Harun Dastekin <hdastekin@gmail.com>
2024-12-05 06:44:38 +0100

    work on content

--- src/components/Home.js
+++ src/components/Home.js
@@ -22,9 +22,20 @@ function Home() {
                 >
                     <div style={{ border: '1px solid #ddd', padding: '1rem', width: '200px', background: '#f9f9f9' }}>
                         {t('home.services')}
+                        {/*<p>IoT-Device Installation und Aktualisierung</p>*/}
+                        <p>{t('home.service1.title')}</p>
+                        <p>{t('home.service2.title')}</p>
+                        <p>{t('home.service3.title')}</p>
+                        <p>{t('home.service4.title')}</p>
+                        <p>{t('home.service5.title')}</p>
                     </div>
-                    <div style={{ border: '1px solid #ddd', padding: '1rem', width: '200px', background: '#f9f9f9' }}>
+                    <div style={{border: '1px solid #ddd', padding: '1rem', width: '200px', background: '#f9f9f9'}}>
                         {t('home.blog')}
+                        <p>{t('home.blog1.title')}</p>
+                        <p>{t('home.blog2.title')}</p>
+                        <p>{t('home.blog3.title')}</p>
+                        <p>{t('home.blog4.title')}</p>
+                        <p>{t('home.blog5.title')}</p>
                     </div>
                 </motion.div>
             </div>
