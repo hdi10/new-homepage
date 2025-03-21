7ac23f769a63b79be120ac8f958356d538e1e81d
Harun Dastekin <hdastekin@gmail.com>
2025-03-21 17:04:26 +0100

    include cypress

--- src/components/Home.js
+++ src/components/Home.js
@@ -89,26 +89,230 @@ function Home() {
                 <div data-aos="zoom-in">
                     <h2>{t('home.welcome')}</h2>
                     <motion.div
-                        style={{ display: 'flex', justifyContent: 'center', gap: '20px' }}
+                        style={{
+                            display: 'grid',
+                            gridTemplateColumns: 'repeat(auto-fit, minmax(220px, 1fr))',
+                            gap: '20px',
+                            justifyItems: 'center'
+                        }}
                         initial={{ opacity: 0 }}
                         animate={{ opacity: 1 }}
                         transition={{ delay: 0.5, duration: 1 }}
                     >
-                        <div style={{ border: '1px solid #ddd', padding: '1rem', width: '200px', background: 'whitesmoke' }}>
-                            {t('home.services')}
-                            <p className={'blogCards'}>{t('home.service1.title')}</p>
-                            <p className={'blogCards'}>{t('home.service2.title')}</p>
-                            <p className={'blogCards'}>{t('home.service3.title')}</p>
-                            <p className={'blogCards'}>{t('home.service4.title')}</p>
-                            <p className={'blogCards'}>{t('home.service5.title')}</p>
-                        </div>
-                        <div style={{ border: '1px solid #ddd', padding: '1rem', width: '200px', background: 'whitesmoke' }}>
-                            {t('home.blog')}
+                        {/*<div style={{ border: '1px solid #ddd',*/}
+                        {/*    padding: '1rem',*/}
+                        {/*    width: '100%',*/}
+                        {/*    maxWidth: '220px',*/}
+                        {/*    background: 'whitesmoke',*/}
+                        {/*    borderRadius: '8px',*/}
+                        {/*    boxShadow: '0 2px 5px rgba(0,0,0,0.05)'*/}
+                        {/*}}>*/}
+                        {/*    {t('home.services')}*/}
+                        {/*    <p className={'blogCards'}>{t('home.service1.title')}</p>*/}
+                        {/*    <p className={'blogCards'}>{t('home.service2.title')}</p>*/}
+                        {/*    <p className={'blogCards'}>{t('home.service3.title')}</p>*/}
+                        {/*    <p className={'blogCards'}>{t('home.service4.title')}</p>*/}
+                        {/*    <p className={'blogCards'}>{t('home.service5.title')}</p>*/}
+                        {/*</div>*/}
+                        {/*<div style={{ border: '1px solid #ddd',*/}
+                        {/*    padding: '1rem',*/}
+                        {/*    width: '100%',*/}
+                        {/*    maxWidth: '220px',*/}
+                        {/*    background: 'whitesmoke',*/}
+                        {/*    borderRadius: '8px',*/}
+                        {/*    boxShadow: '0 2px 5px rgba(0,0,0,0.05)'*/}
+                        {/*}}>*/}
+                        {/*    {t('home.blog')}*/}
+                        {/*    <p className={'blogCards'}>{t('home.blog1.title')}</p>*/}
+                        {/*    <p className={'blogCards'}>{t('home.blog1.description')}</p>*/}
+                        {/*    <p className={'blogCards'}>{t('home.blog2.title')}</p>*/}
+                        {/*    <p className={'blogCards'}>{t('home.blog3.title')}</p>*/}
+                        {/*    <p className={'blogCards'}>{t('home.blog4.title')}</p>*/}
+                        {/*    <p className={'blogCards'}>{t('home.blog5.title')}</p>*/}
+                        {/*</div>*/}
+
+
+                        <div style={{ border: '1px solid #ddd',
+                            padding: '1rem',
+                            width: '100%',
+                            maxWidth: '220px',
+                            background: 'whitesmoke',
+                            borderRadius: '8px',
+                            boxShadow: '0 2px 5px rgba(0,0,0,0.05)'
+                        }}>
+
+
                             <p className={'blogCards'}>{t('home.blog1.title')}</p>
+
+                            <p className={'blogCards'}>{t('home.blog1.description')}</p>
+
+                        </div>
+
+                        <div style={{ border: '1px solid #ddd',
+                            padding: '1rem',
+                            width: '100%',
+                            maxWidth: '220px',
+                            background: 'whitesmoke',
+                            borderRadius: '8px',
+                            boxShadow: '0 2px 5px rgba(0,0,0,0.05)'
+                        }}>
+
+
                             <p className={'blogCards'}>{t('home.blog2.title')}</p>
+
+                            <p className={'blogCards'}>{t('home.blog2.description')}</p>
+
+                        </div>
+
+                        <div style={{ border: '1px solid #ddd',
+                            padding: '1rem',
+                            width: '100%',
+                            maxWidth: '220px',
+                            background: 'whitesmoke',
+                            borderRadius: '8px',
+                            boxShadow: '0 2px 5px rgba(0,0,0,0.05)'
+                        }}>
+
+
                             <p className={'blogCards'}>{t('home.blog3.title')}</p>
+
+                            <p className={'blogCards'}>{t('home.blog3.description')}</p>
+
+                        </div>
+
+                        <div style={{ border: '1px solid #ddd',
+                            padding: '1rem',
+                            width: '100%',
+                            maxWidth: '220px',
+                            background: 'whitesmoke',
+                            borderRadius: '8px',
+                            boxShadow: '0 2px 5px rgba(0,0,0,0.05)'
+                        }}>
+
+
                             <p className={'blogCards'}>{t('home.blog4.title')}</p>
+
+                            <p className={'blogCards'}>{t('home.blog4.description')}</p>
+
+                        </div>
+
+                        <div style={{ border: '1px solid #ddd',
+                            padding: '1rem',
+                            width: '100%',
+                            maxWidth: '220px',
+                            background: 'whitesmoke',
+                            borderRadius: '8px',
+                            boxShadow: '0 2px 5px rgba(0,0,0,0.05)'
+                        }}>
+
+
                             <p className={'blogCards'}>{t('home.blog5.title')}</p>
+
+                            <p className={'blogCards'}>{t('home.blog5.description')}</p>
+
+                        </div>
+
+                        <div style={{ border: '1px solid #ddd',
+                            padding: '1rem',
+                            width: '100%',
+                            maxWidth: '220px',
+                            background: 'whitesmoke',
+                            borderRadius: '8px',
+                            boxShadow: '0 2px 5px rgba(0,0,0,0.05)'
+                        }}>
+
+                            <p className={'blogCards'}>{t('home.service1.title')}</p>
+
+                            <p className={'blogCards'}>{t('home.service1.description')}</p>
+                        </div>
+
+
+
+                        <div style={{
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                            justifyContent: 'flex-start',
+                            border: '1px solid #ddd',
+                            padding: '1rem',
+                            width: '100%',
+                            maxWidth: '360px',
+                            background: 'whitesmoke',
+                            borderRadius: '8px',
+                            boxShadow: '0 2px 5px rgba(0,0,0,0.05)',
+                            margin: '0 auto'
+                        }}>
+                            <p className="blogCards" style={{ marginBottom: '1rem', textAlign: 'center' }}>
+                                {t('home.service2.title')}
+                            </p>
+
+                            <img
+                                src="/images/CypressTesting.gif"
+                                alt="Preview"
+                                style={{
+                                    width: '100%',
+                                    maxHeight: '200px',
+                                    objectFit: 'cover',
+                                    borderRadius: '4px',
+                                    marginBottom: '1rem'
+                                }}
+                            />
+
+                            <p className="blogCards" style={{ textAlign: 'center' }}>
+                                {t('home.service2.description')}
+                            </p>
+                        </div>
+
+
+
+                        <div style={{ border: '1px solid #ddd',
+                            padding: '1rem',
+                            width: '100%',
+                            maxWidth: '220px',
+                            background: 'whitesmoke',
+                            borderRadius: '8px',
+                            boxShadow: '0 2px 5px rgba(0,0,0,0.05)'
+                        }}>
+
+                            <p className={'blogCards'}>{t('home.service3.title')}</p>
+
+                            <p className={'blogCards'}>{t('home.service3.description')}</p>
+
+
+
+                        </div>
+
+                        <div style={{ border: '1px solid #ddd',
+                            padding: '1rem',
+                            width: '100%',
+                            maxWidth: '220px',
+                            background: 'whitesmoke',
+                            borderRadius: '8px',
+                            boxShadow: '0 2px 5px rgba(0,0,0,0.05)'
+                        }}>
+
+
+                            <p className={'blogCards'}>{t('home.service4.title')}</p>
+
+                            <p className={'blogCards'}>{t('home.service4.description')}</p>
+
+
+                        </div>
+
+                        <div style={{ border: '1px solid #ddd',
+                            padding: '1rem',
+                            width: '100%',
+                            maxWidth: '220px',
+                            background: 'whitesmoke',
+                            borderRadius: '8px',
+                            boxShadow: '0 2px 5px rgba(0,0,0,0.05)'
+                        }}>
+
+
+                            <p className={'blogCards'}>{t('home.service5.title')}</p>
+
+                            <p className={'blogCards'}>{t('home.service5.description')}</p>
+
                         </div>
                     </motion.div>
                 </div>
